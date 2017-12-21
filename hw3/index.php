f81fc5699aa3deb5c92e46b07b41647bb0cb0ae7
Afik Bar <afikbar@gmail.com>
2017-12-22 00:48:14 +0200

    add css and table

--- hw3/index.php
+++ hw3/index.php
@@ -1,23 +1,63 @@
 <html>
 <head>
-    <style>
-        th, td {
-            padding: 10px;
-        }
-
-        h1, h2 {
-            text-align: center;
-        }
-
-        body {
-            background-color: tomato;
-        }
-    </style>
-
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <h1>Welcome to TedTalks Information System!</h1>
 <h2> Here is our generic descriptions, WOW!</h2>
+<div style="text-align:center;"><img src="ted.gif" style="width:600px;">
+    <br>
+    <a href="submit_talk.php">Submit a new Talk</a>
+</div>
+<br>
+<div class="datagrid" style="max-width:400px; margin:0 auto;">
+    <table>
+        <thead>
+        <tr>
+            <th>Talk's Title</th>
+            <th>Popularity</th>
+        </tr>
+        </thead>
+        <tfoot>
+        <tr>
+            <td colspan="2">
+                <div id="paging">
+                    <ul>
+                        <li><a href="#"><span>Previous</span></a></li>
+                        <li><a href="#" class="active"><span>1</span></a></li>
+                        <li><a href="#"><span>2</span></a></li>
+                        <li><a href="#"><span>3</span></a></li>
+                        <li><a href="#"><span>4</span></a></li>
+                        <li><a href="#"><span>5</span></a></li>
+                        <li><a href="#"><span>Next</span></a></li>
+                    </ul>
+                </div>
+        </tr>
+        </tfoot>
+        <tbody>
+        <tr>
+            <td>data</td>
+            <td>data</td>
+        </tr>
+        <tr class="alt">
+            <td>data</td>
+            <td>data</td>
+        </tr>
+        <tr>
+            <td>data</td>
+            <td>data</td>
+        </tr>
+        <tr class="alt">
+            <td>data</td>
+            <td>data</td>
+        </tr>
+        <tr>
+            <td>data</td>
+            <td>data</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
 
 
 </body>
