9175cc52953dc2a8ffdffb89cb3145b2653cb5fb
Afik Bar <afikbar@gmail.com>
2017-12-23 12:24:12 +0200

    add debug to batch

--- hw3/batch_submit.php
+++ hw3/batch_submit.php
@@ -19,31 +19,59 @@ if (isset($_POST["submit"])) {
         echo "error";
         die(print_r(sqlsrv_errors(), true));
     }
-//echo "connected to DB"; //debug
-    $file = $_FILES['csv']['tmp_name'];
-    $csvAsArray = array_map('str_getcsv', file($file));
+    echo "connected to DB<br>"; //debug
+    echo $_FILES[csv][error]."<br>";
+    echo $_FILES[csv][name]."<br>";
+    echo $_FILES[csv][tmp_name]."<br>";
+    $file = $_FILES[csv][tmp_name];
+    echo "Importing file: ";
+    echo $file."<br>";
+    echo "file($_FILES[csv][tmp_name])";
+    echo "<br>";
+    $csvAsArray = array_map('str_getcsv', file($_FILES[csv][tmp_name]));
+    echo "$csvAsArray";
     $header = array_shift($csvAsArray);
     $csv = array();
-    foreach ($csvAsArray as $row) {
-        $csv[] = array_combine($header, $row);
+    echo "before CSV create<br>";
+    if (($handle = fopen($file, "r")) !== FALSE) {
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            $sql = "INSERT INTO Ted(name, main_speaker, description, event, languages, speaker_occupation, url, duration, comments, views) 
+            VALUES  ('" . addslashes($data[7]) . "',
+                     '" . addslashes($data[6]) . "',
+                     '" . addslashes($data[1]) . "',
+                     '" . addslashes($data[3]) . "',
+                     '" . addslashes($data[5]) . "',
+                     '" . addslashes($data[12]) . "',
+                     '" . addslashes($data[15]) . "',
+                     '" . addslashes($data[2]) . "',
+                     '" . addslashes($data[0]) . "',
+                     '" . addslashes($data[16]) . "'); 
+             ";
+            sqlsrv_query($conn, $sql);
+        }
+        fclose($handle);
     }
 
-    foreach ($csv as $row) {
-        echo "$row";
-        $sql = "INSERT INTO Ted(name, main_speaker, description, event, languages,
-                                speaker_occupation, url, duration, comments, views)
-                VALUES ('".addslashes($row['name'])."',
-                        '".addslashes($row['main_speaker'])."',
-                        '".addslashes($row['description'])."',
-                        '".addslashes($row['event'])."',
-                        '".addslashes($row['languages'])."', 
-                        '".addslashes($row['speaker_occupation'])."',
-                        '".addslashes($row['url'])."',
-                        '".addslashes($row['duration'])."', 
-                        '".addslashes($row['comments'])."',
-                        '".addslashes($row['views'])."');";
-        sqlsrv_query($conn, $sql);
-    }
+//    foreach ($csvAsArray as $row) {
+//        echo "$row<br>";
+//        $csv[] = array_combine($header, $row);
+//    }
+//    echo "$csv";
+//    foreach ($csv as $row) {
+//        $sql = "INSERT INTO Ted(name, main_speaker, description, event, languages,
+//                                speaker_occupation, url, duration, comments, views)
+//                VALUES ('".addslashes($row['name'])."',
+//                        '".addslashes($row['main_speaker'])."',
+//                        '".addslashes($row['description'])."',
+//                        '".addslashes($row['event'])."',
+//                        '".addslashes($row['languages'])."',
+//                        '".addslashes($row['speaker_occupation'])."',
+//                        '".addslashes($row['url'])."',
+//                        '".addslashes($row['duration'])."',
+//                        '".addslashes($row['comments'])."',
+//                        '".addslashes($row['views'])."');";
+//        sqlsrv_query($conn, $sql);
+//    }
 }
 ?>
 <div class="card" style="text-align: left; width: auto">
